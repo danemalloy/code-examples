47c509fb769c52822f663dbd0beb00802927e1f3
dane malloy <123224237+danemalloy@users.noreply.github.com>
2024-06-11 17:28:06 -0700

    Update DataHandler.ts

--- DataHandler.ts
+++ DataHandler.ts
@@ -1,54 +1,87 @@
-/* eslint-disable roblox-ts/no-array-pairs */
-/* eslint-disable roblox-ts/lua-truthiness */
-
 // written by dane1up
 
-// imports
-import profileService from "@rbxts/rbx-profileservice-plus";
+//imports
+import { Service, OnStart, OnInit } from "@flamework/core";
+import { Players } from "@rbxts/services";
+import ProfileService from "@rbxts/rbx-profileservice-plus";
 import { Profile } from "@rbxts/rbx-profileservice-plus/out/globals";
-import { template } from "./dataTemplate";
-import { doLeaderstats } from "./doLeaderstats";
-import { PlayerDataType } from "server/serverInterfaces";
+import { PLAYER_DATA_TEMPLATE } from "./template";
+import { PlayerDataType } from "server/types/data";
 
 // settings
-const _DATA_KEY = "Testing_v1.0.1";
-const _DATA_TEMPLATE = template;
-const _PROFILE_STORE = profileService.GetProfileStore(_DATA_KEY, _DATA_TEMPLATE);
-const _PROFILES = new Map<Player, Profile<PlayerDataType>>();
+const DATA_KEY = "Testing_v1.0.0";
+let PROFILE_STORE = ProfileService.GetProfileStore(DATA_KEY, PLAYER_DATA_TEMPLATE);
+const PROFILES = new Map<Player, Profile<PlayerDataType>>();
 
-// class
-export class DaneStoreService {
+@Service({})
+export class DaneStoreService implements OnStart, OnInit {
+	onInit() {
+		PROFILE_STORE = PROFILE_STORE.Mock;
+	}
+
+	onStart() {
+		Players.PlayerAdded.Connect((player: Player) => {
+			this.onPlayerAdded(player);
+		});
+		Players.PlayerRemoving.Connect((player: Player) => {
+			this.onPlayerRemoving(player);
+		});
+	}
+
 	onPlayerAdded(player: Player) {
-		const profile = _PROFILE_STORE.LoadProfile(`player_${player.UserId}`, "ForceLoad");
+		const userId = player.UserId;
+		const profile = PROFILE_STORE.LoadProfile("player_" + userId);
+
+		if (!profile) {
+			player.Kick("Error loading data profile. Please try again.");
+			return;
+		}
 
 		if (!player.IsDescendantOf(game)) {
 			profile?.Release();
 			return;
 		}
 
-		if (profile === undefined) {
-			player.Kick("Failed to load data! Please rejoin or try again later.");
-			return;
-		}
-		profile.AddUserId(player.UserId);
+		profile.AddUserId(userId);
 		profile.Reconcile();
 
-		_PROFILES.set(player, profile as unknown as Profile<PlayerDataType>);
+		PROFILES.set(player, profile as Profile<PlayerDataType>);
 
-		doLeaderstats(player, _PROFILES.get(player) as unknown as Profile<PlayerDataType>);
+		this.makeLS(player);
 	}
 
 	onPlayerRemoving(player: Player) {
-		const profile = _PROFILES.get(player);
-		if (profile === undefined) {
+		const profile = PROFILES.get(player);
+
+		if (!profile) {
 			return;
 		}
 
 		profile.Release();
-		_PROFILES.delete(player);
+		PROFILES.delete(player);
+	}
+
+	makeLS(player: Player) {
+		const profile = PROFILES.get(player);
+
+		if (!profile) {
+			warn("cannot make leaderstats, player has no profile");
+			return;
+		}
+
+		const leaderstats = new Instance("Folder");
+		leaderstats.Name = "leaderstats";
+		leaderstats.Parent = player;
+
+		for (const [STAT_NAME, STAT_VALUE] of pairs(profile.Data.Stats)) {
+			const value = new Instance("StringValue");
+			value.Name = STAT_NAME;
+			value.Value = tostring(STAT_VALUE);
+			value.Parent = leaderstats;
+		}
 	}
 
 	async getProfile(player: Player) {
-		return _PROFILES.get(player);
+		return PROFILES.get(player);
 	}
 }
